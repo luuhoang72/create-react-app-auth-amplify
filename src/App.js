0f66373a4c7197493d74a2dc91c0fc429a04760e
luuhoang72 <luuhoang72@gmail.com>
2022-08-10 15:34:12 +0700

    Update App.js
    fix <p>Hello from me, Hoang Luu</p>

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ class App extends Component {
         <AmplifySignOut />
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Hello from me, Hoang Luu<p>
+          <p>Hello from me, Hoang Luu</p>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
